008df5e2f565c00922fa1faa00701f936d65a3e8
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-24 21:44:34 +0300

    2 done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryListEl = document.querySelector('.gallery');
+
+const galleryItemsEl = createGalery(galleryItems);
+
+gallaryListEl.innerHTML = galleryItemsEl;
+
+function createGalery(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title= "${description}"/>
+</a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
